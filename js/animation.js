ca8857bab0f77009302905821d6ca66849c2c0e8
we_nicholas <nicholas@wecast.com.br>
2020-02-05 23:08:17 -0300

    Codigo otimizado

--- js/animation.js
+++ js/animation.js
@@ -34,35 +34,11 @@
       }
     });
     
-    $('a[href*="#"]')
-  		.not('[href="#"]')
-  		.not('[href="#0"]')
-  		.click(function(event) {
-    		// On-page links
-    		if (
-      		location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
-      		&& 
-      		location.hostname == this.hostname
-    		) {
-      		var target = $(this.hash);
-      		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      		if (target.length) {
-        		event.preventDefault();
-        		$('html, body').animate({
-          		scrollTop: target.position().top - (($( window ).width() < 992)? 290 : 60)
-        		}, 1000, function() {
-
-          		var $target = $(target);
-          		$target.focus();
-          		if ($target.is(":focus")) {
-            		return false;
-          		} else {
-            		$target.attr('tabindex','-1');
-            		$target.focus();
-          		};
-        		});
-      		}
-    		}
+    $('a[href*="#"]').click(function(event) {
+          event.preventDefault();
+          $('html, body').animate({
+            scrollTop: $(this.hash).position().top - (($( window ).width() < 992)? 290 : 60)
+          }, 1000);
   		});
 
   });
