06575821eb0d451fe08801731a476beda7a77c40
Nicholas Teixeira Soares <teixeirasoaresnicholas@gmail.com>
2022-11-04 11:00:08 -0300

    feat: new about me section, and headers.

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -6,9 +6,9 @@ import profileImageURL from '@/assets/img/profile.jpg';
 
 <template>
   <ThemeControl />
-  <main class="text-gray-700 dark:text-gray-100 transition-colors">
+  <main class="text-gray-700 dark:text-gray-100">
     <div class="container mx-auto">
-      <div class="grid grid-cols-1 gap-0 transition-colors md:grid-cols-3 md:gap-3">
+      <div class="grid grid-cols-1 gap-0 md:grid-cols-3 md:gap-3">
         <article>
           <div class="p-3 m-3 bg-gray-200 dark:bg-slate-600 rounded-lg shadow-lg">
             <div class="flex flex-wrap justify-center">
@@ -42,149 +42,163 @@ import profileImageURL from '@/assets/img/profile.jpg';
               </p>
             </aside>
           </div>
+          <div class="p-3 mx-3 mb-1 mt-4 bg-gray-200 dark:bg-slate-600 rounded-lg shadow-lg">
+            <h1 class="mt-2 pb-1 text-2xl text-center text-gray-700 dark:text-gray-100 border-b border-gray-400">
+              About Me
+            </h1>
+            <p class="mt-2 text-center text-gray-700 dark:text-gray-100">
+              I started programming at the age of 12 as a hobby, and today I already have a long and successful career in the IT area,
+              always aiming to bring new ideas and best practices to the market.
+            </p>
+          </div>
         </article>
         <div class="col-span-2 p-3 m-3 bg-gray-200 dark:bg-slate-800 rounded-lg shadow-lg lg:p-10 lg:pt-3">
-          <article class="border-b-2 border-gray-500">
-            <h1 class="mt-8 text-xl text-left text-gray-800 dark:text-gray-200">
-              Tech Lead <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2021 - Present</span>
-            </h1>
-            <p class="pl-2 text-lg">
-              Dicas Mei/MeiShop
-            </p>
-            <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
-              <FontAwesomeIcon icon="location-dot" /> Bauru - SP, Brazil
-            </p>
-            <p class="pt-6">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-              sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p class="pt-3">
-              Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">Laravel, Synfony, React Native, Vue, React, Redis and MySql.</span>
-            </p>
-            <p class="py-3">
-              Public Projects:
-              <a href="https://app.meunegocio.meishop.com.br/">
-                <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Meu Negocio</span>
-              </a>,
-              <a href="https://meishop.com.br/">
-                <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Mei Shop</span>
-              </a>,
-              <a href="https://wiser.dicasmei.com.br/">
-                <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Wiser Dicas Mei</span>
-              </a>,
-              <a href="https://natura.dicasmei.com.br/">
-                <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Natura Dicas Mei</span>
-              </a>
-            </p>
-          </article>
-          <article class="border-b-2 border-gray-500">
-            <h1 class="mt-8 text-xl text-left text-gray-800 dark:text-gray-200">
-              Software Architect <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2020 - 2021</span>
-            </h1>
-            <p class="pl-2 text-lg">
-              Atlas Technologies
-            </p>
-            <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
-              <FontAwesomeIcon icon="location-dot" /> Pelotas - RS, Brazil
-            </p>
-            <p class="pt-6">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-              sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p class="pt-3">
-              Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">Laravel, Vue, Redis and MySql.</span>
-            </p>
-            <p class="py-3">
-              Public Projects:
-              <a href="https://fatalmodel.com/">
-                <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Fatal Model</span>
-              </a>
-            </p>
-          </article>
-          <article class="border-b-2 border-gray-500">
-            <h1 class="mt-8 text-xl text-left text-gray-700 dark:text-gray-100">
-              Full Stack Developer <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2019 - 2020</span>
-            </h1>
-            <p class="pl-2 text-lg">
-              WeCast
-            </p>
-            <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
-              <FontAwesomeIcon icon="location-dot" /> Rio Grande - RS, Brazil
-            </p>
-            <p class="pt-6">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-              sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p class="pt-3">
-              Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">Django, NodeJs, Flutter, Vue, React and MySql.</span>
-            </p>
-            <p class="py-3">
-              Public Projects:
-              <a href="https://multimarcas.online/">
-                <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Multimarcas.online</span>
-              </a>
-            </p>
-          </article>
-          <article class="border-b-2 border-gray-500">
-            <h1 class="mt-8 text-xl text-left text-gray-700 dark:text-gray-100">
-              Full Stack Developer <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2018 - 2018</span>
-            </h1>
-            <p class="pl-2 text-lg">
-              Compasso LTDA
-            </p>
-            <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
-              <FontAwesomeIcon icon="location-dot" /> Rio Grande - RS, Brazil
-            </p>
-            <p class="pt-6">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-              sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p class="py-2">
-              Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">NodeJs, React Native, React and MySql.</span>
-            </p>
-          </article>
           <article>
-            <h1 class="mt-8 text-xl text-left text-gray-700 dark:text-gray-100">
-              Full Stack Developer <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2015 - Present</span>
+            <h1 class="mt-2 pb-1 text-center text-2xl text-gray-700 dark:text-gray-100 border-b border-gray-400">
+              About My Career
             </h1>
-            <p class="pl-2 text-lg">
-              Soares Technologies
-            </p>
-            <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
-              <FontAwesomeIcon icon="location-dot" /> Florianopolis - SC, Brazil
-            </p>
-            <p class="pt-6">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
-              sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </p>
-            <p class="py-2">
-              Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">Laravel, NodeJs, Vue, React, React Native, Redis and MySql.</span>
-            </p>
+            <aside class="border-b-2 border-gray-500">
+              <h1 class="mt-8 text-xl text-left text-gray-800 dark:text-gray-200">
+                Tech Lead <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2021 - Present</span>
+              </h1>
+              <p class="pl-2 text-lg">
+                Dicas Mei/MeiShop
+              </p>
+              <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
+                <FontAwesomeIcon icon="location-dot" /> Bauru - SP, Brazil
+              </p>
+              <p class="pt-6">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+              <p class="pt-3">
+                Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">Laravel, Synfony, React Native, Vue, React, Redis and MySql.</span>
+              </p>
+              <p class="py-3">
+                Public Projects:
+                <a href="https://app.meunegocio.meishop.com.br/">
+                  <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Meu Negocio</span>
+                </a>,
+                <a href="https://meishop.com.br/">
+                  <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Mei Shop</span>
+                </a>,
+                <a href="https://wiser.dicasmei.com.br/">
+                  <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Wiser Dicas Mei</span>
+                </a>,
+                <a href="https://natura.dicasmei.com.br/">
+                  <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Natura Dicas Mei</span>
+                </a>
+              </p>
+            </aside>
+            <aside class="border-b-2 border-gray-500">
+              <h1 class="mt-8 text-xl text-left text-gray-800 dark:text-gray-200">
+                Software Architect <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2020 - 2021</span>
+              </h1>
+              <p class="pl-2 text-lg">
+                Atlas Technologies
+              </p>
+              <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
+                <FontAwesomeIcon icon="location-dot" /> Pelotas - RS, Brazil
+              </p>
+              <p class="pt-6">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+              <p class="pt-3">
+                Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">Laravel, Vue, Redis and MySql.</span>
+              </p>
+              <p class="py-3">
+                Public Projects:
+                <a href="https://fatalmodel.com/">
+                  <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Fatal Model</span>
+                </a>
+              </p>
+            </aside>
+            <aside class="border-b-2 border-gray-500">
+              <h1 class="mt-8 text-xl text-left text-gray-700 dark:text-gray-100">
+                Full Stack Developer <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2019 - 2020</span>
+              </h1>
+              <p class="pl-2 text-lg">
+                WeCast
+              </p>
+              <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
+                <FontAwesomeIcon icon="location-dot" /> Rio Grande - RS, Brazil
+              </p>
+              <p class="pt-6">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+              <p class="pt-3">
+                Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">Django, NodeJs, Flutter, Vue, React and MySql.</span>
+              </p>
+              <p class="py-3">
+                Public Projects:
+                <a href="https://multimarcas.online/">
+                  <span class="pl-1 text-sm text-gray-600 dark:text-gray-50 underline">Multimarcas.online</span>
+                </a>
+              </p>
+            </aside>
+            <aside class="border-b-2 border-gray-500">
+              <h1 class="mt-8 text-xl text-left text-gray-700 dark:text-gray-100">
+                Full Stack Developer <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2018 - 2018</span>
+              </h1>
+              <p class="pl-2 text-lg">
+                Compasso LTDA
+              </p>
+              <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
+                <FontAwesomeIcon icon="location-dot" /> Rio Grande - RS, Brazil
+              </p>
+              <p class="pt-6">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+              <p class="py-2">
+                Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">NodeJs, React Native, React and MySql.</span>
+              </p>
+            </aside>
+            <aside>
+              <h1 class="mt-8 text-xl text-left text-gray-700 dark:text-gray-100">
+                Full Stack Developer <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">2015 - Present</span>
+              </h1>
+              <p class="pl-2 text-lg">
+                Soares Technologies
+              </p>
+              <p class="pl-2 text-sm text-gray-700 dark:text-gray-100">
+                <FontAwesomeIcon icon="location-dot" /> Florianopolis - SC, Brazil
+              </p>
+              <p class="pt-6">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+                when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+                It has survived not only five centuries, but also the leap into electronic typesetting,
+                remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset
+                sheets containing Lorem Ipsum passages, and more recently with desktop publishing software
+                like Aldus PageMaker including versions of Lorem Ipsum.
+              </p>
+              <p class="py-2">
+                Technologies Used: <span class="pl-1 text-sm text-gray-600 dark:text-gray-50">Laravel, NodeJs, Vue, React, React Native, Redis and MySql.</span>
+              </p>
+            </aside>
           </article>
         </div>
       </div>
